9a5f08459593dd642fa9fad1aed27bbc94c43c0b
pastoreli <pastoreli.igor@yahoo.com>
2019-03-11 10:38:42 -0300

    add: a new project

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+    
+    <div class="side-bar">
+      <h1 class="title">My App</h1>
+
+      <div class="menu">
+        <a class="menu-item">Menu Link 1</a>
+        <a class="menu-item">Menu Link 2</a>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="header"></div>
+
+      <div class="content">
+        <div class="card-content">
+          <div class="card shadow-1">
+            <h3 class="title">Card 1</h3>
+            <p class="value">19000</p>
+          </div>
+          <div class="card shadow-1">
+            <h3 class="title">Card 2</h3>
+            <p class="value">14000</p>
+          </div>
+          <div class="card shadow-1">
+            <h3 class="title">Card 3</h3>
+            <p class="value">15000</p>
+          </div>
+          <div class="card shadow-1">
+            <h3 class="title">Card 4</h3>
+            <p class="value">12345</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+</template>
